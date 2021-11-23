f85bb0c2fbbbdbbe1dd26660c9e256171bc4b2c4
joaquin.mancini <andrx_it@hotmail.com>
2021-11-23 03:52:04 -0300

    desarrollo del componente de listado de ventas

--- despensaF/src/app/component/sales/sales.component.ts
+++ despensaF/src/app/component/sales/sales.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Sale
+} from 'src/app/models/sale/sale.model';
+import {
+  SaleService
+} from 'src/app/services/sale/sale.service';
+import {
+  MatDialog
+} from "@angular/material/dialog";
+import {
+  SaleCreateFormComponent
+} from './sale-create-form/sale-create-form.component';
 
 @Component({
   selector: 'app-sales',
@@ -7,9 +22,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesComponent implements OnInit {
 
-  constructor() { }
+  public sales: Sale[];
+
+  constructor(private _sale: SaleService,
+    private dialog: MatDialog) {
+      this.sales = [];
+    }
 
   ngOnInit(): void {
+    this._sale.getSales()
+      .subscribe(data => this.sales = data.sort((a, b) => a.idSale!-b.idSale!));
   }
+
+  //Actions
+  //Creation of product
+  // create() {
+  //   const dialogRef = this.dialog.open(SaleCreateFormComponent, {});
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     if (result) {
+  //       this._sale.createSale(result)
+  //         .subscribe(
+  //           response => {
+  //             dialogRef.close();
+  //           },
+  //           error => {
+  //             console.log(error);
+  //           });
+  //       this.sales.push(result);
+  //     }
+  //   });
+  // }
 
 }
